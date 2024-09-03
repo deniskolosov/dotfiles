97bdaa4fa47405bb9637d6f32169611b30ea0c14
Denis Kolosov <denis@audiencerepublic.com>
2024-09-03 21:36:25 +0300

    Format nvim config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -48,108 +48,176 @@ return {
 			local lspconfig = require("lspconfig")
 			local wk = require("which-key")
 			local builtin = require("telescope.builtin")
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-        callback = function(event)
-          -- In this case, we create a function that lets us more easily define mappings specific
-          -- for LSP related items. It sets the mode, buffer and description for us each time.
-          local map = function(keys, func, desc, mode)
-            mode = mode or 'n'
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-          end
-          -- Jump to the definition of the word under your cursor.
-          --  This is where a variable was first declared, or where a function is defined, etc.
-          --  To jump back, press <C-t>.
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+				callback = function(event)
+					-- In this case, we create a function that lets us more easily define mappings specific
+					-- for LSP related items. It sets the mode, buffer and description for us each time.
+					local map = function(keys, func, desc, mode)
+						mode = mode or "n"
+						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+					-- Jump to the definition of the word under your cursor.
+					--  This is where a variable was first declared, or where a function is defined, etc.
+					--  To jump back, press <C-t>.
+					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 
-          -- Find references for the word under your cursor.
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+					-- Find references for the word under your cursor.
+					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
-          -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+					-- Jump to the implementation of the word under your cursor.
+					--  Useful when your language has ways of declaring types without an actual implementation.
+					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
-          -- Jump to the type of the word under your cursor.
-          --  Useful when you're not sure what type a variable is and you want to see
-          --  the definition of its *type*, not where it was *defined*.
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+					-- Jump to the type of the word under your cursor.
+					--  Useful when you're not sure what type a variable is and you want to see
+					--  the definition of its *type*, not where it was *defined*.
+					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
-          -- Fuzzy find all the symbols in your current document.
-          --  Symbols are things like variables, functions, types, etc.
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+					-- Fuzzy find all the symbols in your current document.
+					--  Symbols are things like variables, functions, types, etc.
+					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 
-          -- Fuzzy find all the symbols in your current workspace.
-          --  Similar to document symbols, except searches over your entire project.
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+					-- Fuzzy find all the symbols in your current workspace.
+					--  Similar to document symbols, except searches over your entire project.
+					map(
+						"<leader>ws",
+						require("telescope.builtin").lsp_dynamic_workspace_symbols,
+						"[W]orkspace [S]ymbols"
+					)
 
+					-- The following two autocommands are used to highlight references of the
+					-- word under your cursor when your cursor rests there for a little while.
+					--    See `:help CursorHold` for information about when this is executed
+					--
+					-- When you move your cursor, the highlights will be cleared (the second autocommand).
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+						local highlight_augroup =
+							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							buffer = event.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.document_highlight,
+						})
 
-          -- The following two autocommands are used to highlight references of the
-          -- word under your cursor when your cursor rests there for a little while.
-          --    See `:help CursorHold` for information about when this is executed
-          --
-          -- When you move your cursor, the highlights will be cleared (the second autocommand).
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							buffer = event.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.clear_references,
+						})
 
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.clear_references,
-            })
+						vim.api.nvim_create_autocmd("LspDetach", {
+							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+							callback = function(event2)
+								vim.lsp.buf.clear_references()
+								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+							end,
+						})
+					end
 
-            vim.api.nvim_create_autocmd('LspDetach', {
-              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-              callback = function(event2)
-                vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-              end,
-            })
-          end
-
-          -- The following code creates a keymap to toggle inlay hints in your
-          -- code, if the language server you are using supports them
-          --
-          -- This may be unwanted, since they displace some of your code
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[T]oggle Inlay [H]ints')
-          end
-        end,
-      })
+					-- The following code creates a keymap to toggle inlay hints in your
+					-- code, if the language server you are using supports them
+					--
+					-- This may be unwanted, since they displace some of your code
+					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+						map("<leader>th", function()
+							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+						end, "[T]oggle Inlay [H]ints")
+					end
+				end,
+			})
 
 			local setup_lsp_keybindings = function()
-        wk.add({
-          { "<leader>l", group = "LSP" },
-          { "<leader>ld", vim.lsp.buf.definition, desc = "Go to Definition" },
-          { "<leader>fg",       "<cmd>lua require('telescope.builtin').live_grep({ cwd = '~/.local/share/nvim/gp/chats' })<cr>",   desc = "Search GPT files" },
-          -- Jump to the definition of the word under your cursor.
-          --  This is where a variable was first declared, or where a function is defined, etc.
-          --  To jump back, press <C-t>.
-          { "<leader>ld", "<cmd>lua require('telescope.builtin').lsp_definitions<cr>" , desc = "Go to Definition" },
-          { "<leader>lD", vim.lsp.buf.declaration, desc = "Go to Declaration" },
-          { "<leader>lh", vim.lsp.buf.hover, desc = "Show Hover Information" },
-          { "<leader>lH", vim.lsp.buf.signature_help, desc = "Show Signature Help" },
-          { "<leader>lr", vim.lsp.buf.references, desc = "Find References" },
-          { "<leader>la", vim.lsp.buf.code_action, desc = "List Code Actions" },
-          { "<leader>lR", vim.lsp.buf.rename, desc = "Rename Symbol" },
-          { "<leader>lf", vim.diagnostic.open_float, desc = "Show Line Diagnostics" },
-          { "<leader>l=", vim.lsp.buf.format, desc = "Format buffer" },
-          { "<leader>E", vim.diagnostic.open_float, desc = "Show Line Diagnostics" },
-          { "<leader>ln", vim.diagnostic.goto_next, desc = "Go to Next Diagnostic" },
-          { "<leader>lp", vim.diagnostic.goto_prev, desc = "Go to Previous Diagnostic" },
+				wk.add({
+					{ "<leader>l", group = "LSP" },
+					{
+						"<leader>ld",
+						vim.lsp.buf.definition,
+						desc = "Go to Definition",
+					},
+					{
+						"<leader>fg",
+						"<cmd>lua require('telescope.builtin').live_grep({ cwd = '~/.local/share/nvim/gp/chats' })<cr>",
+						desc = "Search GPT files",
+					},
+					-- Jump to the definition of the word under your cursor.
+					--  This is where a variable was first declared, or where a function is defined, etc.
+					--  To jump back, press <C-t>.
+					{
+						"<leader>ld",
+						vim.lsp.buf.definition,
+						desc = "Go to Definition",
+					},
+					{
+						"<leader>lD",
+						vim.lsp.buf.declaration,
+						desc = "Go to Declaration",
+					},
+					{
+						"<leader>lh",
+						vim.lsp.buf.hover,
+						desc = "Show Hover Information",
+					},
+					{
+						"<leader>lH",
+						vim.lsp.buf.signature_help,
+						desc = "Show Signature Help",
+					},
+					{
+						"<leader>lr",
+						vim.lsp.buf.references,
+						desc = "Find References",
+					},
+					{
+						"<leader>la",
+						vim.lsp.buf.code_action,
+						desc = "List Code Actions",
+					},
+					{
+						"<leader>lR",
+						vim.lsp.buf.rename,
+						desc = "Rename Symbol",
+					},
+					{
+						"<leader>lf",
+						vim.diagnostic.open_float,
+						desc = "Show Line Diagnostics",
+					},
+					{
+						"<leader>l=",
+						vim.lsp.buf.format,
+						desc = "Format buffer",
+					},
+					{
+						"<leader>E",
+						vim.diagnostic.open_float,
+						desc = "Show Line Diagnostics",
+					},
+					{
+						"<leader>ln",
+						vim.diagnostic.goto_next,
+						desc = "Go to Next Diagnostic",
+					},
+					{
+						"<leader>lp",
+						vim.diagnostic.goto_prev,
+						desc = "Go to Previous Diagnostic",
+					},
 
-          -- Fuzzy find all the symbols in your current workspace.
-          { "<leader>ls", builtin.lsp_document_symbols, desc = "Lsp Document Symbols" },
-          --  Similar to document symbols, except searches over your entire project.
-          { "<leader>lS", builtin.lsp_dynamic_workspace_symbols, desc = "Lsp Document Symbols" },
-        }, { prefix = "<leader>" })
+					-- Fuzzy find all the symbols in your current workspace.
+					{
+						"<leader>ls",
+						builtin.lsp_document_symbols,
+						desc = "Lsp Document Symbols",
+					},
+					--  Similar to document symbols, except searches over your entire project.
+					{
+						"<leader>lS",
+						builtin.lsp_dynamic_workspace_symbols,
+						desc = "Lsp Document Symbols",
+					},
+				}, { prefix = "<leader>" })
 			end
 
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
