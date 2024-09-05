83c7dadc6a4f788e92ca02e5177b186a9c30c50a
Denis Kolosov <denis@audiencerepublic.com>
2024-09-05 23:06:42 +0300

    Remove tab mapping to free it for copilot

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -26,7 +26,7 @@ return {
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<tab>'] = cmp.mapping.complete(),
+          -- ['<tab>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
