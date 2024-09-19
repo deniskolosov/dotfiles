afe505c5889b76b38c00fe848f8917b9078c0985
Denis Kolosov <denis@audiencerepublic.com>
2024-09-19 21:32:20 +0400

    Format go files

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -3,9 +3,11 @@ return {
   config = function()
     local null_ls = require('null-ls')
     null_ls.setup({
-      filetypes = {"javascript", "typescript", "html", "css"},
+      filetypes = {"javascript", "typescript", "html", "css", "go"},
       sources = {
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.goimports_reviser,
       },
     })
   end
