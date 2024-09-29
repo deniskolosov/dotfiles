add8ce7d5e32f152a73a2dfbe9e08a06d7456828
Denis Kolosov <d.kolosov@apibank.ru>
2024-09-29 17:31:20 +0400

    Configure ruff lsp server

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -47,55 +47,45 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-      -- vim.lsp.set_log_level("debug")
+			-- vim.lsp.set_log_level("debug")
 			local lspconfig = require("lspconfig")
 			local wk = require("which-key")
 			local builtin = require("telescope.builtin")
+
 			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+				group = vim.api.nvim_create_augroup("lsp_attach_combined", { clear = true }),
 				callback = function(event)
-					-- In this case, we create a function that lets us more easily define mappings specific
-					-- for LSP related items. It sets the mode, buffer and description for us each time.
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if client == nil then
+						return
+					end
+
+					-- Disable hover in favor of Pyright for Ruff
+					if client.name == "ruff" then
+						client.server_capabilities.hoverProvider = false
+						return
+					end
+
+					-- Create mapping function for LSP related bindings
 					local map = function(keys, func, desc, mode)
 						mode = mode or "n"
 						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
-					-- Jump to the definition of the word under your cursor.
-					--  This is where a variable was first declared, or where a function is defined, etc.
-					--  To jump back, press <C-t>.
+
+					-- LSP key mappings
 					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
-					-- Find references for the word under your cursor.
 					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-					-- Jump to the implementation of the word under your cursor.
-					--  Useful when your language has ways of declaring types without an actual implementation.
 					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-
-					-- Jump to the type of the word under your cursor.
-					--  Useful when you're not sure what type a variable is and you want to see
-					--  the definition of its *type*, not where it was *defined*.
 					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-
-					-- Fuzzy find all the symbols in your current document.
-					--  Symbols are things like variables, functions, types, etc.
 					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-
-					-- Fuzzy find all the symbols in your current workspace.
-					--  Similar to document symbols, except searches over your entire project.
 					map(
 						"<leader>ws",
 						require("telescope.builtin").lsp_dynamic_workspace_symbols,
 						"[W]orkspace [S]ymbols"
 					)
 
-					-- The following two autocommands are used to highlight references of the
-					-- word under your cursor when your cursor rests there for a little while.
-					--    See `:help CursorHold` for information about when this is executed
-					--
-					-- When you move your cursor, the highlights will be cleared (the second autocommand).
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+					-- Document highlight
+					if client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
 						local highlight_augroup =
 							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
 						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
@@ -119,16 +109,14 @@ return {
 						})
 					end
 
-					-- The following code creates a keymap to toggle inlay hints in your
-					-- code, if the language server you are using supports them
-					--
-					-- This may be unwanted, since they displace some of your code
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+					-- Inlay hints toggle
+					if client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
 						map("<leader>th", function()
 							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
 						end, "[T]oggle Inlay [H]ints")
 					end
 				end,
+				desc = "LSP: Combined auto commands",
 			})
 
 			local setup_lsp_keybindings = function()
@@ -244,6 +232,16 @@ return {
 				capabilities = capabilities,
 			})
 
+			lspconfig.ruff.setup({
+				init_options = {
+					settings = {
+						-- Ruff language server settings go here
+						args = { "--max-line-length=120", "--select=E,W,F,N" }, -- Example: setting max line length and selecting specific error codes
+						exclude = { "*.pyc", "__pycache__" }, -- Exclude specific patterns, like compiled Python files and caches
+					},
+				},
+			})
+
 			lspconfig.pylsp.setup({
 				on_attach = setup_lsp_keybindings,
 				capabilities = capabilities,
@@ -253,9 +251,7 @@ return {
 						plugins = {
 							ruff = {
 								-- formatter + Linter + isort
-								enabled = true,
-                lineLength = 120,
-								extendSelect = { "I" },
+								disabled = true,
 							},
 							-- formatter options
 							black = { enabled = false },
