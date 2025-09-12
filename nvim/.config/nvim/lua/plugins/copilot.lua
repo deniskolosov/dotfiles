3a387d2465ed96a3b8682c07ee389c655d7b04fa
Denis Kolosov <denis.kolosov@gmail.com>
2025-09-12 22:56:35 +0300

    Disable copilot

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,4 @@
--- return {
+return {
 --   "github/copilot.vim",
 --   config = function ()
 --     vim.api.nvim_set_keymap('n', '<leader>cp', ':Copilot enable<CR>', {noremap = true, silent = true})
@@ -11,58 +11,58 @@
 --   end
 -- }
 --
-return {
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		config = function()
-			require("copilot").setup({
-				panel = {
-					enabled = false,
-					auto_refresh = false,
-					keymap = {
-						jump_prev = "C-p",
-						jump_next = "C-n",
-						accept = "<CR>",
-						refresh = "gr",
-						open = "<M-CR>",
-					},
-					layout = {
-						position = "bottom", -- | top | left | right | horizontal | vertical
-						ratio = 0.4,
-					},
-				},
-				suggestion = {
-					enabled = false,
-					auto_trigger = false,
-					hide_during_completion = true,
-					debounce = 75,
-					keymap = {
-						accept = "<M-\\>",
-						accept_word = false,
-						accept_line = false,
-						next = "<M-]>",
-						prev = "<M-[>",
-						dismiss = "<C-]>",
-					},
-				},
-				filetypes = {
-					yaml = false,
-					markdown = true,
-					help = false,
-					gitcommit = false,
-					gitrebase = false,
-					hgcommit = false,
-					svn = false,
-					cvs = false,
-					["."] = false,
-				},
-				copilot_node_command = "node", -- Node.js version must be > 18.x
-				server_opts_overrides = {},
-			})
-		end,
-	},
+-- return {
+-- 	{
+-- 		"zbirenbaum/copilot.lua",
+-- 		cmd = "Copilot",
+-- 		event = "InsertEnter",
+-- 		config = function()
+-- 			require("copilot").setup({
+-- 				panel = {
+-- 					enabled = false,
+-- 					auto_refresh = false,
+-- 					keymap = {
+-- 						jump_prev = "C-p",
+-- 						jump_next = "C-n",
+-- 						accept = "<CR>",
+-- 						refresh = "gr",
+-- 						open = "<M-CR>",
+-- 					},
+-- 					layout = {
+-- 						position = "bottom", -- | top | left | right | horizontal | vertical
+-- 						ratio = 0.4,
+-- 					},
+-- 				},
+-- 				suggestion = {
+-- 					enabled = false,
+-- 					auto_trigger = false,
+-- 					hide_during_completion = true,
+-- 					debounce = 75,
+-- 					keymap = {
+-- 						accept = "<M-\\>",
+-- 						accept_word = false,
+-- 						accept_line = false,
+-- 						next = "<M-]>",
+-- 						prev = "<M-[>",
+-- 						dismiss = "<C-]>",
+-- 					},
+-- 				},
+-- 				filetypes = {
+-- 					yaml = false,
+-- 					markdown = true,
+-- 					help = false,
+-- 					gitcommit = false,
+-- 					gitrebase = false,
+-- 					hgcommit = false,
+-- 					svn = false,
+-- 					cvs = false,
+-- 					["."] = false,
+-- 				},
+-- 				copilot_node_command = "node", -- Node.js version must be > 18.x
+-- 				server_opts_overrides = {},
+-- 			})
+-- 		end,
+-- 	},
 	{
 		"zbirenbaum/copilot-cmp",
 		event = { "InsertEnter", "LspAttach" },
